cb0ccf57360b1dae492eecfd306c829e95671097
cahogg <cahogg123@gmail.com>
2022-09-02 22:19:04 -0700

    Update 2

--- Scenes/Turrets/Turrets.gd
+++ Scenes/Turrets/Turrets.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 
-
+#allows turrets to turn at set FPS in settings
 func _physics_process(delta):
 	turn()
 		
+#currently lets turrets turn to face the direction of the cursor
 func turn():
 	var enemy_position = get_global_mouse_position()
 	get_node("Turret").look_at(enemy_position)
