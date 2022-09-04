2485563aeb6a8b94aa6bb927c58f1dec91af8758
cahogg <cahogg123@gmail.com>
2022-09-04 14:39:28 -0700

    Shoot Enemies

--- Scenes/Enemies/BlueTank.gd
+++ Scenes/Enemies/BlueTank.gd
@@ -1,9 +1,27 @@
 extends PathFollow2D
 
-var speed = 300
+var speed = 150
+var hp = 50
+
+onready var health_bar = get_node("HealthBar")
+
+func _ready():
+	health_bar.max_value = hp
+	health_bar.value = hp
+	health_bar.set_as_toplevel(true)
 
 func _physics_process(delta):
 	move(delta)
 
 func move(delta):
 	set_offset(get_offset() + speed * delta)
+	health_bar.set_position(position - Vector2(20,30))
+
+func on_hit(damage):
+	hp -= damage
+	health_bar.value = hp
+	if hp <= 0:
+		on_destroy()
+
+func on_destroy():
+	self.queue_free()
--- Scenes/MainScenes/GameScene.gd
+++ Scenes/MainScenes/GameScene.gd
@@ -84,6 +84,7 @@ func verify_and_build():
 		var new_tower = load("res://Scenes/Turrets/" + build_type + ".tscn").instance()
 		new_tower.position = build_location
 		new_tower.built = true
+		new_tower.type = build_type
 		map_node.get_node("Turrets").add_child(new_tower, true)
 		map_node.get_node("TowerExclusion").set_cellv(build_tile, 5)
 		## deduct cash
